d23386f7aeddfca43a395ca8ed1a389c5ab22b4a
sbaisden25 <spbaisden@gmail.com>
2022-07-31 18:28:20 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+// Imports
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Global variables
+const app = express();
+const port = process.env.PORT || 5000;
+
+// CORS
+app.use(cors());
+
+require('dotenv').config();
+
+//backend greeting page
+app.get("/", (req, res) => {
+  res.send("backend server is running");
+});
+
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || process.env.ATLAS_URI, {
+   useNewUrlParser: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error(err));
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+   
+
+const productsRouter = require('./routes/api/products');
+app.use('/products', productsRouter); 
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});  
